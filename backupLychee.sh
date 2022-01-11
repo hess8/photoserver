c4bd81eafeb6109f1003c3cc1e399fb66bdf0a04
Bret Hess <bret.hess@gmail.com>
2022-01-11 12:24:07 -0700

    Added daily copy of database file to bash script

--- backupLychee.sh
+++ backupLychee.sh
@@ -3,3 +3,8 @@ rsync -av --progress --log-file=/home/bret/photoserver/logs/rsync.log \
    /media/sf_backup/photoserverLychee/rsyncLychee
 python3 /home/bret/photoserver/summaryEmailLogs.py
 mv -f /home/bret/photoserver/logs/rsync.log /home/bret/photoserver/logs/copyrsync.log
+## make a database file copy
+_now=$(date +"%Y_%m_%d")
+_databasefile="/media/sf_backup/photoserverLychee/rsyncLychee/Lychee/database/database.sqlite"
+_backupfile="/media/sf_backup/photoserverLychee/databaseDailyCopy/database_$_now.sqlite"
+cp "$_databasefile" "$_backupfile"
